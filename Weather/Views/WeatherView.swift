d0fa4a2269d22fe956d27749b192772eac319f4c
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-03-17 00:13:28 -0300

    feat: integrate WeatherRow

--- Weather/Views/WeatherView.swift
+++ Weather/Views/WeatherView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct WeatherView: View {
     var weather: ResponseBody
-
+    
     
     var body: some View {
         ZStack(alignment: .leading){
@@ -10,32 +10,32 @@ struct WeatherView: View {
                 VStack (alignment: .leading, spacing: 5){
                     Text(weather.name)
                         .bold().font(.title)
-
+                    
                     Text("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
                         .fontWeight(.light)
-
+                    
                     Spacer()
-
+                    
                     VStack {
                         HStack {
                             VStack(spacing: 20) {
                                 Image(systemName: "sun.max")
                                     .font(.system(size: 40))
-
+                                
                                 Text(weather.weather[0].main)
                             }
                             .frame(width: 150, alignment: .leading)
-
+                            
                             Spacer()
-
+                            
                             Text(weather.main.feelsLike.roundDouble() + "°")
                                 .font(.system(size: 100))
                                 .fontWeight(.bold)
                                 .padding()
                         }
-
+                        
                         Spacer()
-
+                        
                         AsyncImage(url: URL(string: "https://cdn.pixabay.com/photo/2020/01/24/21/33/city-4791269_960_720.png")) { image in
                             image
                                 .resizable()
@@ -44,9 +44,9 @@ struct WeatherView: View {
                         } placeholder: {
                             ProgressView()
                         }
-
+                        
                         Spacer()
-
+                        
                     }
                     .frame(maxWidth: .infinity)
                 }
@@ -54,13 +54,26 @@ struct WeatherView: View {
             }
             .padding()
             .frame(maxWidth: .infinity, alignment: .leading)
-
-
+            
+            
             VStack {
                 Spacer()
-
+                
                 VStack(alignment: .leading, spacing: 20) {
-
+                    Text("Weather now")
+                        .bold().padding(.bottom)
+                    
+                    HStack {
+                        WeatherRow(logo: "thermometer", name: "Min temp", value: (weather.main.tempMin.roundDouble() + ("°")))
+                        Spacer()
+                        WeatherRow(logo: "thermometer", name: "Max temp", value: (weather.main.tempMax.roundDouble() + "°"))
+                    }
+                    
+                    HStack {
+                        WeatherRow(logo: "wind", name: "Wind speed", value: (weather.wind.speed.roundDouble() + " m/s"))
+                        Spacer()
+                        WeatherRow(logo: "humidity", name: "Humidity", value: "\(weather.main.humidity.roundDouble())%")
+                    }
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding()
