8af4827d182f4b01d1e8ce6f877ddb05d23a1cdf
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-03-16 23:37:22 -0300

    feat: add previewData

--- Weather/Views/ContentView.swift
+++ Weather/Views/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             if let location = locationManager.location {
 
                 if let weather = weather {
-                    
+                    WeatherView(weather: weather)
                 } else {
                     LoadingView()
                         .task {
---
+++ Weather/Views/WeatherView.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherView.swift
+//  Weather
+//
+//  Created by Gabriel Oliveira on 16/03/22.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var weather: ResponseBody
+
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView(weather: previewWeather)
+    }
+}
